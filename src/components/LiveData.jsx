364243ee3d156a45eabe6ff500afcebbc4ae98ba
ROHIT <kumarohit1202@gmail.com>
2024-12-23 14:41:46 +0530

    refactored

--- src/components/LiveData.jsx
+++ src/components/LiveData.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Html } from '@react-three/drei';
 
-function LiveDataDisplay({ machineData, distanceFactor = 16, position, onDataUpdate, machineName, machineID }) {
+function LiveDataDisplay({ machineData, distanceFactor = 16, position, machineName }) {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
@@ -20,39 +20,7 @@ function LiveDataDisplay({ machineData, distanceFactor = 16, position, onDataUpd
         return 'bg-white/90';
     }
   };
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('http://172.36.0.73:8016/api/GetMachineDetails?machineID=' + machineID);
-  //       if (!response.ok) {
-  //         throw new Error('Network response was not ok');
-  //       }
-  //       const jsonData = await response.json();
-  //       console.log('Fetched data:', jsonData);
-  //       setData(jsonData);
-  //       // Update parent component with AE value
-  //       if (onDataUpdate && jsonData[0]) {
-  //         onDataUpdate(jsonData[0]); // Assuming AE is the first value
-  //       }
-  //       setError(null);
-  //     } catch (err) {
-  //       setError('Failed to fetch data');
-  //       console.error('Error fetching data:', err);
-  //     }
-  //   };
-
-  //   // Initial fetch
-  //   fetchData();
-
-  //   // Set up interval for periodic updates
-  //   const intervalId = setInterval(fetchData, 20000);
-
-  //   // Cleanup interval on unmount
-  //   return () => clearInterval(intervalId);
-  // }, [machineID, onDataUpdate]);
   
-
   return (
     <Html distanceFactor={distanceFactor} position={position}>
       <div className="bg-white/90 p-4 rounded-lg shadow-lg min-w-[350px]">
